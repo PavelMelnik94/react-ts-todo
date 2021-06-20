f2f28179f136a92d3cae2dcc8fe349a6e3eeef75
Pavel <recyclesordie94@gmail.com>
2021-06-20 13:37:08 +0300

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect, useRef } from "react";
+import {ITodo} from '../types/data'
+
+const App: React.FC= () => {
+
+
+
+    return (
+        <div>
+            <div>
+                <input type="text" />
+                <button >Add</button>
+            </div>
+        </div>
+
+    )
+}
+
+export {App}
